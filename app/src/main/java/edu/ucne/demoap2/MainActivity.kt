8a2a680f27531e713f432045e8d46efa160d4e84
fahh7 <fahh7@FREIMYHP>
2025-05-08 13:06:38 -0400

    Creando el registro de tecnicos en android

--- app/src/main/java/edu/ucne/demoap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/demoap2/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.ucne.demoap2.ui.theme.DemoAp2Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +38,50 @@ class MainActivity : ComponentActivity() {
 
 
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Bandera(modifier:Modifier = Modifier) {
+    Column {
+        Row {
+            Column {
+                Cuadrado(Color.Blue)
+                Cuadrado(Color.Red)
+
+            }
+
+        }
+        Row {
+            Column {
+                Cuadrado(Color.Red)
+                Cuadrado(Color.Blue)
+            }
+
+        }
+
+    }
+}
+
+
+@Composable
+
+fun Cuadrado(Color:Int){
+    ElevatedCard(
+        shape= RoundedCornerShape(8.dp)
+        modifier = Modificar
+         .padding()
+         .size(width = 200.dp height = 100.dp)
+        .backgroundColor(Color=Color.Red)
+    ) {
+
+    }
+
+}
+
+
+fun BanderaPreview() {
     DemoAp2Theme {
 
     }
 }
+
+
+
